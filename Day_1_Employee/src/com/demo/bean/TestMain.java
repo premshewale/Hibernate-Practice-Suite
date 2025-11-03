7d84aca4da3c3154db330be0af9f1db01a9bab4a
Prem Shewale <premshewale47@gmail.com>
2025-11-03 11:00:44 +0530

    added First Day of Hibernate Practice

---
+++ Day_1_Employee/src/com/demo/bean/TestMain.java
@@ -0,0 +1,28 @@
+package com.demo.bean;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Configuration cfg=new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		Session s= sf.openSession();
+		Transaction t=s.beginTransaction();
+		Employee e1=new Employee(12, "ABC", 20000);
+		s.save(e1);
+		t.commit();
+		s.close();
+		sf.close();
+		System.out.println("All done check DB..");
+		
+	}
+
+}
