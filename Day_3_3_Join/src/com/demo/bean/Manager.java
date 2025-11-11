6c98c8af12bdd4e20b3f2eaf93555750d7bd75b2
Prem Shewale <premshewale47@gmail.com>
2025-11-11 12:01:59 +0530

    added practice set

---
+++ Day_3_3_Join/src/com/demo/bean/Manager.java
@@ -0,0 +1,53 @@
+package com.demo.bean;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MgrEmpJoin22july23")
+@PrimaryKeyJoinColumn(name="empno")
+public class Manager extends Employee{
+  
+	private double incentive;
+
+	public double getIncentive() {
+		return incentive;
+	}
+
+	public void setIncentive(double incentive) {
+		this.incentive = incentive;
+	}
+
+	
+
+	public Manager(int empno, String name, double salary) {
+		super(empno, name, salary);
+	}
+
+	public Manager(String name, double salary, double incentive) {
+		super(name, salary);
+		this.incentive = incentive;
+	}
+
+	public Manager(double incentive) {
+		super();
+		this.incentive = incentive;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [incentive=" + incentive + ", toString()=" + super.toString() + "]";
+	}
+
+	
+	
+	
+	
+	
+}
