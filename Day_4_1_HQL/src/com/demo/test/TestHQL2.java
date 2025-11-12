779f29d06d01d5498716f25a9f7a27fcaa0d919a
Prem Shewale <premshewale47@gmail.com>
2025-11-12 12:19:58 +0530

    added day 4 parctice

---
+++ Day_4_1_HQL/src/com/demo/test/TestHQL2.java
@@ -0,0 +1,45 @@
+package com.demo.test;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.bean.Product;
+
+public class TestHQL2 {
+
+Session s;
+	
+	public TestHQL2(){
+		Configuration cfg=new Configuration();
+		cfg.configure();
+		SessionFactory sf= cfg.buildSessionFactory();
+		s=sf.openSession();
+	}
+		public void ShowProduct(){
+			Transaction t= s.beginTransaction();
+			Query query =s.createQuery("select ProdName,Category,Price from Product");
+			List list=query.list();
+			
+			for(int i=0;i<list.size();i++){
+				
+				Object obj[]=(Object[])list.get(i);
+				System.out.println(obj[0]+" "+obj[1]+" "+obj[2]);
+			}
+				
+			t.commit();
+			s.close();
+		}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestHQL2 t1= new TestHQL2();
+		t1.ShowProduct();
+	}
+
+}
